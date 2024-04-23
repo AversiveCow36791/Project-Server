bac1f3071e8e34eca0bd4a8d5834524821322f11
Hudson7777 <1155124383@link.cuhk.edu.hk>
2024-04-23 19:49:55 -0400

    Try to add the server side files for questions, but files are not verified yet

---
+++ Kanbas/questions/schema.js
@@ -0,0 +1,24 @@
+import mongoose, { Mongoose } from "mongoose";
+const questionSchema = new mongoose.Schema({
+    title: {type: String,},
+    points: Number,
+    type: {
+        type: String,
+        enum: ['Fill in multiple blanks', 'True/false', 'MultipleChoice'],
+        default: 'MultipleChoice'
+    },
+    options: {
+        type: [String],
+        default: []
+    },
+    answers: {
+        type: [mongoose.SchemaTypes.Mixed],
+    },
+    question: {
+        type: String,
+    },
+    quiz: {
+        type: String
+    }
+},{collection:"questions"})                                               
+export default questionSchema;
